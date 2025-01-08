202a207191d70d8dd65b7a810043384b69acdd88
WillStonebridge <WStonebridge1@gmail.com>
2025-01-08 13:32:21 -0600

    changed svg text to p

--- src/Introduction.tsx
+++ src/Introduction.tsx
@@ -5,15 +5,9 @@ function Introduction() {
     <div id="intro_background">
         <div id="introduction">
           <img src="/bridge.svg"></img>
-          <svg viewBox="0 0 150 19">
-              <text x="0" y="14">I'm Will Stonebridge,</text>
-          </svg>
-          <svg viewBox="0 0 162 17">
-              <text x="0" y="12">I solve problems using</text>
-          </svg>
-          <svg viewBox="0 0 284 18">
-              <text x="0" y="13">Data Science and Software Engineering</text>
-          </svg>
+          <p id="line1">I'm Will Stonebridge</p>
+          <p id="line2">I solve problems using</p>
+          <p id="line3">Data Science and Software Engineering</p>
       </div>
       </div>
   )
