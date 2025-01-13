c063f762d2b06e5bf87a4fd177c0b0e55722fb25
WillStonebridge <WStonebridge1@gmail.com>
2025-01-13 18:27:17 -0500

    replaced intro text with an image, why did I not think of this earlier

--- src/Introduction.tsx
+++ src/Introduction.tsx
@@ -5,7 +5,8 @@ function Introduction() {
     <div id="intro_background">
         <div id="introduction">
           <img src="/bridge.svg"></img>
-          <svg viewBox="0 0 150 19">
+          <img src='/intro_text.png'></img>
+          {/* <svg viewBox="0 0 150 19">
               <text x="0" y="14">I'm Will Stonebridge,</text>
           </svg>
           <svg viewBox="0 0 162 17">
@@ -13,7 +14,7 @@ function Introduction() {
           </svg>
           <svg viewBox="0 0 284 18">
               <text x="0" y="13">Data Science and Software Engineering</text>
-          </svg>
+          </svg> */}
       </div>
       </div>
   )
